4e80028b5f1c1e51d7f14f3933c4e6b79174c6c0
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-03-19 20:04:51 +0100

    Update rectangle.cpp

--- lab_3/rectangle.cpp
+++ lab_3/rectangle.cpp
@@ -1,29 +1,30 @@
 #include "rectangle.h"
 #include <cmath>
+#include <array>
 
-Rectangle::Rectangle(std::vector<Line> l) {
-    setLines(l);
+Rectangle::Rectangle(std::vector<Line> l) { 
+    setLines(l); 
 }
 
 float Rectangle::calcArea() {
     std::vector<Line> l = getLines();
-    if (l.size() < 2) return 0;
-
-    Point* p1 = l[0].getPoints();
-    float len1 = std::sqrt((p1[1].getX() - p1[0].getX()) * (p1[1].getX() - p1[0].getX()) + (p1[1].getY() - p1[0].getY()) * (p1[1].getY() - p1[0].getY()));
-
-    Point* p2 = l[1].getPoints();
-    float len2 = std::sqrt((p2[1].getX() - p2[0].getX()) * (p2[1].getX() - p2[0].getX()) + (p2[1].getY() - p2[0].getY()) * (p2[1].getY() - p2[0].getY()));
+    if (l.size() < 2) return 0.0f;
+    
+    std::array<Point, 2> pts1 = l[0].getPoints();
+    float len1 = std::sqrt((pts1[1].getX() - pts1[0].getX()) * (pts1[1].getX() - pts1[0].getX()) + (pts1[1].getY() - pts1[0].getY()) * (pts1[1].getY() - pts1[0].getY()));
+                           
+    std::array<Point, 2> pts2 = l[1].getPoints();
+    float len2 = std::sqrt((pts2[1].getX() - pts2[0].getX()) * (pts2[1].getX() - pts2[0].getX()) + (pts2[1].getY() - pts2[0].getY()) * (pts2[1].getY() - pts2[0].getY()));
 
     return len1 * len2;
 }
 
 float Rectangle::calcCircum() {
     std::vector<Line> l = getLines();
-    float sum = 0;
+    float sum = 0.0f;
     for (size_t i = 0; i < l.size(); ++i) {
-        Point* p = l[i].getPoints();
-        sum += std::sqrt((p[1].getX() - p[0].getX()) * (p[1].getX() - p[0].getX()) + (p[1].getY() - p[0].getY()) * (p[1].getY() - p[0].getY()));
+        std::array<Point, 2> pts = l[i].getPoints();
+        sum += std::sqrt((pts[1].getX() - pts[0].getX()) * (pts[1].getX() - pts[0].getX()) + (pts[1].getY() - pts[0].getY()) * (pts[1].getY() - pts[0].getY()));
     }
     return sum;
 }
